f6b0b23a232768c4116220bc8f95e4c5015bc6ea
JiroFg <mechcromancer@gmail.com>
2022-10-20 13:00:48 -0500

    ejercicio del 20/10/22

--- practica06/upload/src/main/java/mx/uv/App.java
+++ practica06/upload/src/main/java/mx/uv/App.java
@@ -1,9 +1,10 @@
 package mx.uv;
+import static spark.Spark.*;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        get("/hello", (req, res) -> "Hello World");
     }
 }
